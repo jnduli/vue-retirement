597eec21af6a1459f67a06c6c498245104f9a9cb
John Nduli <yohanaizraeli@gmail.com>
2019-05-28 14:38:47 +0300

    Fix simpleinterest tests

--- tests/unit/calculations/SimpleInterest.spec.js
+++ tests/unit/calculations/SimpleInterest.spec.js
@@ -1,4 +1,4 @@
-import { organizeSIMonthlyInvestments, getSIAccumulatedAmount, getSIMonthlyProfit } from '@/calculations/SimpleInterest.js'
+import { organizeSIMonthlyInvestments, getSIAccumulatedAmount } from '@/calculations/SimpleInterest.js'
 
 describe('SimpleInterest Calculations', () => {
   it('organizes SI interest to monthly investments', () => {
@@ -8,13 +8,15 @@ describe('SimpleInterest Calculations', () => {
         interest_type: 'simple',
         interest: 10.00,
         distribution: 'constant',
-        percentage: 10
+        percentage: 10,
+        starting_amount: 0
       }, {
         type: 'randomThing',
         interest_type: 'simple',
         interest: 15.00,
         distribution: 'constant',
-        percentage: 20
+        percentage: 20,
+        starting_amount: 1000
       }
     ]
 
@@ -25,13 +27,19 @@ describe('SimpleInterest Calculations', () => {
         interest: 10 / 1200,
         principal: [0],
         accumulatedInterest: 0,
-        profit: [0]
+        profit: [0],
+        starting_amount: 0,
+        type: 'random',
+        interest_type: 'simple'
       }, {
         invest: (20 * salary) / 100,
         interest: 15 / 1200,
         principal: [0],
         accumulatedInterest: 0,
-        profit: [0]
+        profit: [0],
+        starting_amount: 1000,
+        type: 'randomThing',
+        interest_type: 'simple'
       }
     ]
     const computedResult = organizeSIMonthlyInvestments(investments, salary)
@@ -46,16 +54,22 @@ describe('SimpleInterest Calculations', () => {
         interest: 10 / 1200,
         principal: [0],
         accumulatedInterest: 0,
-        profit: [0]
+        profit: [0],
+        starting_amount: 0,
+        type: 'random',
+        interest_type: 'simple'
       }, {
         invest: (20 * salary) / 100,
         interest: 15 / 1200,
         principal: [0],
         accumulatedInterest: 0,
-        profit: [0]
+        profit: [0],
+        starting_amount: 1000,
+        type: 'randomThing',
+        interest_type: 'simple'
       }
     ]
     const accumulatedAmount = getSIAccumulatedAmount(investments, 3)
-    expect(accumulatedAmount).toEqual(9200)
+    expect(accumulatedAmount).toEqual(10237.50)
   })
 })
