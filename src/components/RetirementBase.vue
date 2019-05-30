a1921ba86e69b848755241776bc377be60b4668f
John Nduli <yohanaizraeli@gmail.com>
2019-05-30 14:40:45 +0300

    Clean up unused functions and variables

--- src/components/RetirementBase.vue
+++ src/components/RetirementBase.vue
@@ -59,15 +59,14 @@
 
           Add select form on investments made
         <b-modal :active.sync="isAddInvestmentModalActive" has-modal-card>
-            <add-investment-modal :use-percent='use_percent' v-on:addInvestment="addInvestment" :salary="salary"></add-investment-modal>
+            <add-investment-modal v-on:addInvestment="addInvestment" :salary="salary"></add-investment-modal>
         </b-modal>
           <button id="add-investment" v-on:click="isAddInvestmentModalActive = true">Add</button>
         </div>
         <investments-table
-                  :investments="investments"
-                  :use-percent="fake_use_percent"
-                  v-on:remove-investment='removeInvestment'
-                  />
+          :salary="salary"
+          :investments="investments"
+          v-on:remove-investment='removeInvestment' />
       </div>
       <footer class="card-footer">
         <button v-on:click="calculatePeriods" class="card-footer-item">Calculate</button>
@@ -82,7 +81,6 @@
       <div>You have {{ Math.floor(lineData.length/12) }} Years
         and {{ lineData.length%12 }} Months to retire</div>
       <LineChart :line_data="lineData" :tooltip_data="toolTipData"/>
-      <!-- <div v-for="prin in lineData">{{ prin }}</div> -->
     </div>
   </div>
 </template>
@@ -118,71 +116,18 @@ export default {
         currency: 0,
         use_percent: true
       },
-      // expenses: 50,
-      expenses_unit: '%',
       death: Infinity,
       investments: [],
       lineData: [],
       toolTipData: [],
       done_calculating: false,
-      use_percent: true,
-      fake_use_percent: true,
-      use_percent_text: 'Percent %',
       error: false,
       error_message: ''
     }
   },
-  watch: {
-    use_percent (val) {
-      if (val) {
-        this.use_percent_text = 'Percent %'
-        return
-      }
-      this.use_percent_text = 'Percent %'
-      // this.use_percent_text = 'Ksh'
-    }
-  },
-  computed: {
-    label_expenses_unit: function () {
-      if (this.expenses.use_percent) {
-        return '%'
-      }
-      return 'Ksh'
-    },
-    label_monthly: function () {
-      if (this.use_percent) {
-        return '% used on Monthly Expenditure'
-      }
-      return 'Ksh used per month'
-    },
-    label_retire: function () {
-      if (this.use_percent) {
-        return '% of income used on retirement'
-      }
-      return 'Ksh used per month on retirement'
-    }
-  },
   methods: {
-    changeExpensesUnit () {
-      // if it was showing use_percent, change value to correct number
-      if (this.expenses.use_percent) {
-        this.expenses.currency = this.expenses.percent * this.salary / 100
-      } else {
-        this.expenses.percent = this.expenses.currency * 100 / this.salary
-      }
-      this.expenses.use_percent = !this.expenses.use_percent
-    },
     addInvestment (investment) {
       this.investments.push(investment)
-    },
-    add_investment () {
-      this.investments.push({
-        type: '',
-        interest_type: 'simple',
-        interest: 10.00,
-        distribution: '',
-        percentage: 10
-      })
     },
     removeInvestment (index) {
       this.investments.splice(index, 1)
@@ -192,7 +137,7 @@ export default {
       if (this.death === 0 || this.death === null) {
         this.death = Infinity
       }
-      const result = calculateInvestmentPeriods(JSON.parse(JSON.stringify(this.investments)), this.salary, this.expenses.percent, this.retirement_expenses.percent, true, this.death)
+      const result = calculateInvestmentPeriods(JSON.parse(JSON.stringify(this.investments)), this.salary, this.expenses.percent, this.retirement_expenses.percent, this.death)
       const passed = result.passed
       if (!passed) {
         this.error = true
